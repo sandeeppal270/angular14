2851d3bbac3eb9bcd10644b4a959996b259f093e
sandeeppal270 <sandeeppal270@gmail.com>
2023-02-26 23:55:33 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-project';
+
+@HostListener('click',['$event'])
+  show(){
+    alert('alert of hostlistner');
+  }
+
+
 }
